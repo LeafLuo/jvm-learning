3a3cd0fd4ca0ef39b5abe0285ee77b3d66ffc143
Gao <gaoxinyu17@gmail.com>
2018-04-06 20:37:37 +0800

    Update

--- jvm-baisc/src/main/java/com/leafluo/clazz/Main.java
+++ jvm-baisc/src/main/java/com/leafluo/clazz/Main.java
@@ -1,8 +1,41 @@
 package com.leafluo.clazz;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodType;
+
+import static java.lang.invoke.MethodHandles.lookup;
+
 public class Main {
 
     public static void main(String[] args) {
-        int[][][] array = new int[1][0][-1];
+//        int[][][] array = new int[1][0][-1];
+        (new Main()).new Son().thinking();
+    }
+
+    class GrandFather {
+        void thinking() {
+            System.out.println("i am grandfather");
+        }
+    }
+
+    class Father extends GrandFather {
+        @Override
+        void thinking() {
+            System.out.println("i am father");
+        }
+    }
+
+    class Son extends Father {
+        @Override
+        void thinking() {
+            try {
+                MethodType mt = MethodType.methodType(void.class);
+                MethodHandle mh = lookup().findSpecial(GrandFather.class, "thinking", mt, getClass());
+                mh.invoke(this);
+
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
